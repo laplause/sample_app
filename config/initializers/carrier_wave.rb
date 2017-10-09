d8bb818f4e0cea4a2b1f787067ac80f0dc7aa833
tasku12 <tasku12@gmail.com>
2017-10-09 06:38:47 +0000

    Another go

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJIKGW5P3XDMSTYGA'],
-      :aws_secret_access_key => ENV['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'],
-      :region                => ENV['us-west-2']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['chris-pictures']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
