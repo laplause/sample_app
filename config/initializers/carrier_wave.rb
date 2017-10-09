d3f2d154536dc63f43229d17789f6e43cf658b1e
tasku12 <tasku12@gmail.com>
2017-10-09 06:30:24 +0000

    New aws credentials

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAI64XAMVZDE26344A'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['AKIAJIKGW5P3XDMSTYGA'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['chris-pictures']
   end
